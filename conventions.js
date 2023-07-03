bed825bf58017ca0e91bdb8dfc465cf0cea45426
usualpro <usualpro@gmail.com>
2023-07-03 12:46:48 +0200

    Create conventions.js

---
+++ conventions.js
@@ -0,0 +1,111 @@
+export const conventions = {
+  1: {
+    code: "LPPS",
+    desc: "Fournisseur hors optique / audio"
+  },
+  2: {
+    code: "AMO",
+    desc: "Orthophonie"
+  },
+  3: {
+    code: "AMP",
+    desc: "Pédicure podologue"
+  },
+  4: {
+    code: "CSTD"
+  },
+  I: {
+    code: "AMI",
+    desc: "Infirmier(e)"
+  },
+  K: {
+    code: "AMM",
+    desc: "Kinésithérapie"
+  },
+  U: {
+    code: "CURE"
+  },
+  Y: {
+    code: "AMY",
+    desc: "Orthoptie"
+  },
+
+  A: {
+    code: "AUDI",
+    desc: "Audioprothèse"
+  },
+
+  C: {
+    code: "CSTE",
+    desc: "Centre de santé"
+  },
+  D: {
+    code: "DENT",
+    desc: "Dentaire"
+  },
+  E: {
+    code: "EXTE",
+    desc: "Soins externes hospitaliers"
+  },
+  H: {
+    code: "HOSP",
+    desc: "Séjour hospitalier"
+  },
+  L: {
+    code: "LABO",
+    desc: "Laboratoire"
+  },
+
+  G: {
+    code: "MEDG",
+    desc: "Médecine générale"
+  },
+  S: {
+    code: "MEDS",
+    desc: "Médecine spéc sf radio"
+  },
+  O: {
+    code: "OPTI",
+    desc: "Optique"
+  },
+  P: {
+    code: "PHAR",
+    desc: "Pharmacie"
+  },
+  R: {
+    code: "RADL",
+    desc: "Radiologie"
+  },
+  F: {
+    code: "SAGE",
+    desc: "Sage Femme"
+  },
+  T: {
+    code: "TRAN",
+    desc: "Transport"
+  }
+};
+
+export const conventionTypesByMedicalDomain = {
+  LPPS: [],
+  AMO: [],
+  AMP: [],
+  CSTD: [],
+  AUDI: [],
+  CSTE: [],
+  DENT: [],
+  EXTE: [],
+  SAGE: [],
+  MEDG: [],
+  HOSP: [],
+  AMI: [],
+  AMM: [],
+  LABO: [],
+  OPTI: [],
+  PHAR: [],
+  RADL: [],
+  MEDS: [],
+  TRAN: [],
+  CURE: [],
+  AMY: []
+};
